1cfecf7437c6e68c5d33edbcc377afa3b7fdcd95
Lana <lana.j.vuleta@gmail.com>
2023-03-15 14:29:08 -0400

    Add details to corrections, simplify process
    Combine existing detection limit functions into one.
    Convert UV 254 to Abs 1cm.
    Handle issues with excel read-in: calculated values are read in with excess decimal places. Round incoming calculated values.
    Alter parameter update such that it considers the switch away from measuring using UV absorbance in 2002, removes experimental E.coli measurements, and correctly identifies free and combined chlorine as Clearwell values.

--- R/corrections.R
+++ R/corrections.R
@@ -1,15 +1,17 @@
 #' Convert biological data
 #'
-#' @param labdat labdat file
+#' Identifies biological values stored in per mL by identifying `10^-3` in the
+#'  parameters name and converts the values to per L by multiplying by 1000.
 #'
-#' @importFrom magrittr %>%
-#' @importFrom dplyr mutate
+#' @param labdat dataframe containing the parsed lab data
 #'
-#' @return converted values
-#' @export
+#' @return dataframe containing the parsed lab data with the required biological
+#'  data values and units converted to per L as required
 convert_biocounts <- function(labdat){
+
   parm_list <- c("Blue Green Algae (x10^-3)",
                  "Crustaceans (x10^-3)",
+                 "Diatoms (x10^-3)",
                  "Flagellates (x10^-3)",
                  "Green Algae (x10^-3)",
                  "Nematodes (x10^-3)",
@@ -17,111 +19,176 @@ convert_biocounts <- function(labdat){
                  "Rotifers (x10^-3)")
 
   df <- labdat %>%
-    mutate(result = ifelse(parameter %in% parm_list & result > 0,
-                           result * 1000, result),
-           result_flag = ifelse(grepl(" \\(x10\\^-3\\)", parameter),
+    mutate(result      = ifelse(parameter %in% parm_list,
+                                result * 1000, result),
+           result_flag = ifelse(parameter %in% parm_list,
                                 "converted", result_flag),
-           parameter = ifelse(parameter %in% parm_list,
-                              gsub(" \\(x10\\^-3\\)", "", parameter),
-                              as.character(parameter)))
+           unit        = ifelse(parameter %in% parm_list,
+                                "per litre", unit),
+           parameter   = ifelse(parameter %in% parm_list,
+                                gsub(" \\(x10\\^-3\\)", "", parameter),
+                                as.character(parameter)))
 
-  #df$result_flag[which(grepl(pattern = "\\(x10\\^-3\\)", x = df$parameter))] <- "converted"
   return(df)
 
 }
 
+#' Convert UV 254 values.
+#'
+#' Convert UV 254 values from Abs 10cm to Abs 1cm by dividing values by 10
+#'
+#' @inheritParams convert_biocounts
+#'
+#' @return dataframe containing the parsed lab data with the required UV 254
+#'  data values and units converted to Abs 1cm as required
+convert_UV254 <- function(labdat) {
 
-#' Replace all values above and below detection limit
+  df <- labdat %>%
+    mutate(result      = ifelse(parameter == "UV 254",
+                                result / 10, result),
+           result_flag = ifelse(parameter == "UV 254",
+                                "converted", result_flag),
+           unit        = ifelse(parameter == "UV 254",
+                                "Abs 1cm", unit),
+           parm_unit   = ifelse(parameter == "UV 254",
+                                "UV254_abs.1cm", parm_unit))
+
+  return(df)
+}
+
+#' Round values stored in result_org and result
 #'
-#' @param labdat
+#' Round values stored in result_org and result as indicated in the process
+#'  document. Required as there exists an issue with number storage such that
+#'  when values are read in, some have excess decimal places. Especially
+#'  pertinent for values calculated within the lab data sheet
 #'
-#' @return
-#' @export
+#' @inheritParams convert_biocounts
+#'
+#' @return dataframe containing the parsed lab data where result_org and result
+#'  are recorded with the correct number of decimal points
+round_values <- function(labdat) {
+
+  df <- labdat %T>%
+    {options(warn = -1)} %>%
+          # This mutation handles the issues with number storage
+    mutate(result_org = ifelse(grepl("\\d{1,}\\.\\d{5}", result_org),
+                               as.character(as.numeric(result_org)),
+                               result_org),
+           # At this point, there exist more numbers with many decimal places
+           # because of the sheet's calculated values. These are each rounded
+           # to have the same number of digits are are displayed in the labdats.
+           # grepl statement is used so as to only replace digits and not
+           # strings such as "uncalculable"
+           result_org = ifelse(grepl("\\d{1,}\\.\\d{1}", result_org),
+                               case_when(
+                                 parameter == "Percent DO" |
+                                   parameter == "Total Green & B-G" |
+                                   parameter == "Total Chlorine Dose" |
+                                   parameter == "Turbidity Log Removal"
+                                 ~ as.character(round(as.numeric(result_org), 1)),
+                                 parameter == "Langelier Saturation Index 2" |
+                                   parameter == "Aluminum (particulate)" |
+                                   parameter == "Alum to DOC ratio" |
+                                   parameter == "Anion Sum" |
+                                   parameter == "Cation Sum" |
+                                   parameter == "Ion Percent Difference" |
+                                   parameter == "Langelier Index (RTW)"
+                                 ~ as.character(round(as.numeric(result_org), 2)),
+                                 # Removal values are stored as digits but
+                                 # displayed as percent (0.75 vs 75%)
+                                 grepl("Removal -", parameter)
+                                 ~ as.character(round(as.numeric(result_org), 3)),
+                                 grepl("SUVA", parameter)
+                                 ~ as.character(round(as.numeric(result_org), 4)),
+                                 parameter == "Total THMs" |
+                                   parameter == "Dissolved Solids"
+                                 ~ as.character(round(as.numeric(result_org), 0)),
+                                 TRUE ~ result_org),
+                               result_org),
+           # Same issue as above (calculated values have many decimal places)
+           # exists in the results column, but there are no strings to save
+           result     = case_when(parameter == "Percent DO" |
+                                   parameter == "Total Green & B-G" |
+                                   parameter == "Total Chlorine Dose" |
+                                   parameter == "Turbidity Log Removal"
+                                 ~ round(result, 1),
+                                 parameter == "Langelier Saturation Index 2" |
+                                   parameter == "Aluminum (particulate)" |
+                                   parameter == "Alum to DOC ratio" |
+                                   parameter == "Anion Sum" |
+                                   parameter == "Cation Sum" |
+                                   parameter == "Ion Percent Difference" |
+                                   parameter == "Langelier Index (RTW)"
+                                 ~ round(result, 2),
+                                 grepl("Removal -", parameter)
+                                 ~ round(result, 3),
+                                 grepl("SUVA", parameter)
+                                 ~ round(result, 4),
+                                 parameter == "Total THMs" |
+                                   parameter == "Dissolved Solids"
+                                 ~ round(result, 0),
+                                 TRUE ~ result)) %T>%
+    {options(warn = 0)}
+
+  return(df)
+}
+
+#' Handle values above and below detection limit, asterisked data
+#'
+#' Handle values as defined below:
+#' * If value contains `<`, value is below detection limit. Flag and replace with 0
+#' * If value contains `>`, value is above detection limit. Flag and replace with NA
+#' * If value contains an asterisk, value is flagged with `mod` and replaced with NA
+#'
+#' @inheritParams convert_biocounts
+#'
+#' @return dataframe containing the parsed lab data where the result column and
+#'  flag column are altered as specified above
 replace_dl <- function(labdat){
-  labdat <- convert_adl(labdat = labdat)
-  labdat <- convert_bdl(labdat = labdat)
-  labdat <- convert_ast(labdat = labdat)
+  dl <- c("[<]", "[>]", "TNTC", "[*]")
+
+  labdat <- labdat %>%
+    mutate(result_flag = ifelse(grepl("[<]",  result_org), "bdl", result_flag),
+           result_flag = ifelse(grepl("[>]",  result_org), "adl", result_flag),
+           result_flag = ifelse(grepl("TNTC", result_org), "adl", result_flag),
+           result_flag = ifelse(grepl("[*]",  result_org), "mod", result_flag)) %>%
+    mutate(result      = ifelse(grepl("[<]",  result_org), 0,     result),
+           result      = ifelse(grepl("[>]",  result_org), NA,    result),
+           result      = ifelse(grepl("TNTC", result_org), NA,    result),
+           result      = ifelse(grepl("[*]",  result_org), NA,    result))
 
   return(labdat)
 }
 
-
-#' Replace below detection limit values with
+#' Update parameter names and units
 #'
-#' @param labdat labdat datafile
-#' @param replacement default = 0
+#' Update parameter names and units as indicated in `parameters.csv`. There
+#'  exist inconsistencies between years in parameter names and units, including
+#'  human error. Update to ensure consistency
 #'
-#' @return
-#' @export
-convert_bdl <- function(labdat, replacement = "0"){
-  labdat$result[which(grepl(pattern = "[<]", x = labdat$result))] <- replacement
-
-  labdat$result_flag[which(grepl(pattern = "[<]",
-                                 x = labdat$result_org))] <- "bdl"
-
-  return(labdat)
-
-}
-
-#' Replace above detection limit values
+#' @inheritParams convert_biocounts
+#' @inheritParams prepare_labdat
+#' @param file_sheet_year numeric value indicating the sheet year
 #'
-#' @param labdat labdat datafile
-#' @param replacement value to replace adl values (default = NA)
-#'
-#' @return
-#' @export
-convert_adl <- function(labdat, replacement = NA){
-  labdat$result[which(grepl(pattern = "[>]", x = labdat$result))] <- replacement
-  labdat$result[which(grepl(pattern = "TNTC", x = labdat$result))] <- replacement
+#' @return data frame with updated parameter name, unit, and parm_unit
+update_parameters <- function(labdat, file_sheet_year,
+                              path_to_parameters){
 
-  labdat$result_flag[which(grepl(pattern = "[>]", x = labdat$result_org) |
-                             grepl(pattern = "TNTC", x = labdat$result_org))] <- "adl"
+  labdat_parameters <- read.csv(path_to_parameters,
+                                fileEncoding = "ISO-8859-1",
+                                na.strings = "") %>%
+    mutate(tbl_parameter = as.character(tbl_parameter),
+           tbl_unit = as.character(tbl_unit))
 
-  return(labdat)
+  # To simplify column names while the data frame is being used
+  colnames(labdat_parameters) <- str_remove(colnames(labdat_parameters), "tbl_")
 
-}
+  labdat_mod <- labdat %>%
+    left_join(labdat_parameters, by = c("datasheet", "station", "parameter",
+                                        "unit"))
 
-#' Convert asterisked data
-#'
-#' @param labdat
-#'
-#' @return
-#' @export
-convert_ast <- function(labdat){
-  labdat$result <- gsub(pattern = "[*]", replacement = "", x = labdat$result)
-  labdat$result_flag[which(grepl(pattern = "[*]", x = labdat$result_org))] <- "mod"
-
-  return(labdat)
-
-}
-
-
-#' Update parameters to consistent naming convention
-#'
-#' @param labdat routine lab data file
-#' @param parameters_file name of parameters file
-#' @param data_validation option for data validation. Produces data table with extended column names for verification.
-#'
-#' @importFrom dplyr left_join distinct arrange select mutate rename
-#'
-#' @return data frame with updated parameter names
-#' @export
-#'
-#' @examples
-#' # update_parameters(labdat = labdat, parameters_file = "parameters.csv")
-update_parameters <- function(labdat, parameters_file = "parameters.csv",
-                              data_validation = FALSE){
-  dir <- "./data"
-  fpath <- file.path(dir, parameters_file)
-
-  labdat_parameters <- read.csv(fpath, fileEncoding = "ISO-8859-1") %>%
-    mutate(parameter = as.character(parameter),
-           unit = as.character(unit))
-
-  labdat.mod <- labdat  %>%
-    left_join(labdat_parameters, by = c("datasheet","station","parameter","unit"))
-
-  parm_check <- labdat.mod %>%
+  parm_check <- labdat_mod %>%
     filter(is.na(parm_unit)) %>%
     distinct(datasheet, station, parameter, unit) %>%
     arrange(datasheet)
@@ -131,26 +198,48 @@ update_parameters <- function(labdat, parameters_file = "parameters.csv",
   } else {
     print("Rows for which there exist issues with the parameters:")
     print.data.frame(parm_check)
+    stop("Check parameters.csv and ensure that these parameters are handled.", call. = T)
   }
 
-  if(isTRUE(data_validation)){
-    labdat.updated <- labdat.mod %>%
-      select(datasheet, sheet_year, station, datetime_ymd.hms, year, parameter,
-             parameter_updated, unit, unit_updated, parm_unit,
-             parm_eval, parm_tag, result) %>%
-      #filter(parm_eval == "measured") %>%
-      mutate(station = as.factor(station))
+  if (file_sheet_year < 2003) {
+    labdat_mod <- labdat_mod %>%
+      # DOC/TOC values from 1980 to 2002 (inclusive) were measured using the UV
+      # absorbance method (Note that this does NOT include GF diss values).
+      # Adding "(UV)" to the units to indicate the use of this method.
+      # This is not done with the left_join above as DOC/TOC units in the
+      # labdat sheets are not accurate
+      mutate(unit_updated = ifelse(!grepl("GFdiss", parm_unit) &
+                                     (parameter_updated == "DOC" |
+                                        parameter_updated == "TOC"),
+                                   paste(unit_updated, "(UV)", sep = " "),
+                                   unit_updated),
+             parm_unit    = ifelse(!grepl("GFdiss", parm_unit) &
+                                     (parameter_updated == "DOC" |
+                                        parameter_updated == "TOC"),
+                                   paste(parm_unit, "UV", sep = "."),
+                                   parm_unit))
+  }
 
-  } else {
+  if (file_sheet_year < 2018) {
+    # E. coli values before 2018 were measured using an experimental method.
+    # Plant was accredited for E. coli measurements in 2018
+    labdat_mod <- labdat_mod %>%
+      filter(!grepl("E. coli", parameter))
+  }
 
-    labdat.updated <- labdat.mod %>%
-      select(datasheet, sheet_year, station, datetime_ymd.hms,
-             parameter_updated, unit_updated, parm_unit,
-             parm_eval, parm_tag, result) %>%
-      #filter(parm_eval == "measured") %>%
-      rename("parameter" = parameter_updated,
-             "unit" = unit_updated) %>%
-      mutate(station = as.factor(station))}
-  return(labdat.updated)
+  # Free Chlorine and Combined Chlorine are in the Raw section of yearly
+  # spreadsheets but should be stored as Clearwell values
+  labdat_mod <- labdat_mod %>%
+    select(datasheet, sheet_year, station, date_ymd,
+           parameter = parameter_updated, unit = unit_updated,
+           parm_unit, parm_eval, parm_tag, result) %>%
+    mutate(datasheet = ifelse(parameter == "Free Chlorine" |
+                                parameter == "Combined Chlorine",
+                              "ClearWell", datasheet),
+           station   = ifelse(parameter == "Free Chlorine" |
+                                parameter == "Combined Chlorine",
+                              "Clearwell", station))
+
+  return(labdat_mod)
 }
 
