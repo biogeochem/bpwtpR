eae20235c16712b714f73a71118c78598359464c
Lana <lana.j.vuleta@gmail.com>
2023-03-28 13:06:11 -0400

    Update code to work with new db csv output
    Update @param and @import statements.
    Changing decimal places to be set based on db csv decimal places rather than a strict document (harder to incorporate new parameters).

--- R/tables.R
+++ R/tables.R
@@ -1,26 +1,31 @@
 #' Build monthly summary table
 #'
-#' @param df data for table
-#' @param grouping data grouping
+#' @param df dataframe. Data for table
+#' @param grouping string. data grouping
 #' @param sampling_station sampling location
 #' @param year_select year of interest
 #'
-#' @importFrom pander pander
-#' @importFrom lubridate year
-#' @importFrom dplyr filter select
-#' @importFrom tidyr pivot_wider
-#' @importFrom magrittr %>%
+#' @param df dataframe. Data for table, Default: ''
+#' @param grouping string. data grouping, Default:
+#'  c("operation", "physical", "majorConstituents", "traceConstituents",
+#'    "THM", "biological", "bacteriological")
+#' @param sampling_station string. Sampling location, Default: ''
+#' @param year_select numeric. Desired year, Default: NULL
 #'
-#' @return
+#' @return dataframe
 #' @export
 #'
-#' @examples
-#' #
+#' @importFrom pander pander
+#' @importFrom lubridate year
+#' @importFrom dplyr filter select rowwise
+#' @importFrom tidyr pivot_wider
+#' @importFrom magrittr %>%
 summarize_monthly_table <- function(df = "",
-                                    grouping = c("operation","physical",
+                                    grouping = c("operations","physical",
                                                  "majorConstituents", "traceConstituents",
                                                  "THM", "biological","bacteriological"),
-                                    sampling_station = "", year_select = NULL){
+                                    sampling_station = "",
+                                    year_select = NULL){
 
   if(is.null(year_select)){
     year_select = year(Sys.Date())
@@ -28,33 +33,42 @@ summarize_monthly_table <- function(df = "",
 
   df <- df %>%
     filter(parm_tag == grouping & station == sampling_station,
-           year(datetime_ymd.hms) == year_select)
+           year(date_ymd) == year_select) %>%
+    mutate(result = case_when(result == -999999 ~ NA,
+                              TRUE ~ result))
 
-  table_values <- summarize_monthly(labdat = df)
-
-  if(grouping == "biological"){
-    num_format = "e"
-  } else {
-    num_format = "f"
+  # These parameters do not contain numeric values that
+  if (grouping == "operations") {
+    df <- df %>%
+      filter(!grepl("ON", parameter),
+             !grepl("OFF", parameter),
+             !grepl("Intake", parameter))
   }
 
+  df_round <- df %>%
+    rowwise() %>%
+    mutate(decimal_places = decimal_places(result)) %>%
+    group_by(parameter, station, unit, parm_tag) %>%
+    summarise(round_val = max(decimal_places, na.rm = TRUE))
 
-  parameter_digits <- read.csv("./data/parameter_digits.csv", fileEncoding = "ISO-8859-1")
+  table_values <- summarize_monthly(labdat = df) %>%
+    left_join(df_round) %>%
+    rowwise() %>%
+    mutate(monthly_mean = case_when(!is.infinite(round_val) ~
+                                      round(monthly_mean, round_val),
+                                    TRUE ~
+                                      monthly_mean)) %>%
+    select(month, parameter, unit, monthly_mean)
 
-  table_values <- table_values %>%
-    select(month, parameter, unit, `Monthly Mean`) %>%
-    left_join(parameter_digits)
-
-  # correct precision
-  for(i in 1:nrow(table_values)){
-    table_values$format_value[i] <- formatC(table_values$`Monthly Mean`[i],
-                                            digits = table_values$digits[i],
-                                            format = num_format)
+  if (grouping == "biological"){
+    table_values <- table_values %>%
+      mutate(monthly_mean = formatC(monthly_mean, format = "e"))
   }
 
   table_values <- table_values %>%
-    pivot_wider(id_cols = c(parameter, unit), names_from = month, values_from = format_value)
-
+    pivot_wider(id_cols = c(parameter, unit),
+                names_from = month,
+                values_from = monthly_mean)
 
   table_values <- as.matrix(table_values)
   table_values[is.na(table_values)] <- "--"
@@ -62,7 +76,6 @@ summarize_monthly_table <- function(df = "",
 
   caption.text = paste(year_select, "monthly summary table at the ",
                        sampling_station, "sampling location.")
-
 
   pander(table_values,
          justify = c("left", "center", rep("right", ncol(table_values)-2)),
@@ -72,133 +85,124 @@ summarize_monthly_table <- function(df = "",
 }
 
 
+#' Determine number of decimal places
+#'
+#' Determine the number of decimal places of a numeric value
+#'
+#' @param x numeric value
+#'
+#' @return numeric value. The number of decimal places of the value. Does not
+#'  consider 1.00 to have 2 decimal places but 1
+decimal_places <- function(x) {
+
+  if (is.na(x)) {
+    return(NA)
+  } else {
+    if ((x %% 1) != 0) {
+      dec <- nchar(strsplit(as.character(x), ".", fixed = TRUE)[[1]][[2]])
+      if (dec > 5) {
+        # Sometimes there are issues with decimal places (ex R reads in 16 dec
+        # places). This makes sure that we don't have a ridiculous number of decimal
+        # places in final report
+        dec <- 2
+      }
+    } else {
+      return(0)
+    }
+  }
+
+}
+
 #' Build table with historical values
 #'
-#' @param df dataframe
-#' @param year_select year of interest
-#' @param grouping operational grouping
-#' @param sampling_station sampling location
+#' @inheritParams summarize_monthly_table
 #' @param percentiles percentiles of interest
 #' @param by_month group by month (T) or by year (F)
 #' @param month_select month of interest
 #'
+#' @return dataframe
+#' @export
+#'
 #' @importFrom magrittr %>%
-#' @importFrom dplyr filter left_join
+#' @importFrom dplyr filter left_join rowwise mutate_at
 #' @importFrom pander pander
 #' @importFrom lubridate month
-#'
-#' @return
-#' @export
+#' @importFrom tidyselect matches
 group_table_historical <- function(df = "", year_select = "", grouping = "",
                                    sampling_station = "", percentiles = "",
                                    by_month = T, month_select = NULL){
 
-  parameter_digits <- read.csv("./data/parameter_digits.csv", fileEncoding = "ISO-8859-1")
+  df <- df %>%
+    filter(parm_tag == grouping & station == sampling_station) %>%
+    mutate(result = case_when(result == -999999 ~ NA,
+                              TRUE ~ result))
 
-  if(grouping == "biological"){
-    num_format = "e"
-  } else {
-    num_format = "f"
+  # These parameters do not contain numeric values that
+  if (grouping == "operations") {
+    df <- df %>%
+      filter(!grepl("ON", parameter),
+             !grepl("OFF", parameter),
+             !grepl("Intake", parameter))
   }
 
-
-  df <- df %>%
-    filter(parm_tag == grouping & station == sampling_station)
-
+  df_round <- df %>%
+    rowwise() %>%
+    mutate(decimal_places = decimal_places(result)) %>%
+    group_by(parameter, station, unit, parm_tag) %>%
+    summarise(round_val = max(decimal_places, na.rm = TRUE))
 
   # monthly mean
   month_values <- summarize_monthly(labdat = df) %>%
-    filter(month == month_select, year == year_select) %>%
-    left_join(parameter_digits)
-
-  for(i in 1:nrow(month_values)){
-    month_values$`Monthly Mean`[i] <- formatC(month_values$`Monthly Mean`[i],
-                                              digits = month_values$digits[i],
-                                              format = num_format)
-  }
+    left_join(df_round) %>%
+    rowwise() %>%
+    mutate(monthly_mean = case_when(!is.infinite(round_val) ~
+                                      round(monthly_mean, round_val),
+                                    TRUE ~
+                                      monthly_mean)) %>%
+    select(month, parameter, unit, monthly_mean)
 
 
   # historical extremes
   hist_ext <- historical_extremes(df = df, grouping = grouping,
                                   sampling_station = sampling_station,
                                   by_month = by_month) %>%
-    left_join(parameter_digits) %>% data.frame()
+    left_join(df_round) %>%
+    rowwise() %>%
+    mutate_at(c("Min", "Max"),
+              ~ case_when(!is.infinite(round_val) ~ round(.x, round_val),
+                                       TRUE ~ Min))
 
-  for(i in 1:nrow(hist_ext)){
-    hist_ext$Min_format[i] <- formatC(hist_ext$Min[i],
-                                      digits = hist_ext$digits[i],
-                                      format = num_format)
-    hist_ext$Max_format[i] <- formatC(hist_ext$Max[i],
-                                      digits = hist_ext$digits[i],
-                                      format = num_format)
-  }
 
   # historical percentiles
   hist_per <- historical_percentiles(df = df, grouping = grouping,
                                      sampling_station = sampling_station,
                                      percentiles = percentiles,
-                                     by_month = by_month)%>%
-    left_join(parameter_digits) %>% data.frame()
+                                     by_month = by_month) %>%
+    left_join(df_round) %>%
+    mutate_at(vars(matches("%")),
+              ~ case_when(!is.infinite(round_val) ~ round(.x, round_val),
+                          TRUE ~ .x))
 
-  for(i in 1:nrow(hist_per)){
-    hist_per$X10._format[i] <- formatC(hist_per$X10.[i] ,
-                                       digits = hist_per$digits[i],
-                                       format = num_format)
-    hist_per$X25._format[i] <- formatC(hist_per$X25.[i] ,
-                                       digits = hist_per$digits[i],
-                                       format = num_format)
-    hist_per$X50._format[i] <- formatC(hist_per$X50.[i] ,
-                                       digits = hist_per$digits[i],
-                                       format = num_format)
-    hist_per$X75._format[i] <- formatC(hist_per$X75.[i] ,
-                                       digits = hist_per$digits[i],
-                                       format = num_format)
-    hist_per$X90._format[i] <- formatC(hist_per$X90.[i] ,
-                                       digits = hist_per$digits[i],
-                                       format = num_format)
-  }
 
   # historical basics
   hist_bas <- historical_basics(df = df, grouping = grouping,
                                 sampling_station = sampling_station,
                                 by_month = by_month) %>%
-    left_join(parameter_digits) %>%
-    data.frame() %>%
-    mutate(Mean = as.numeric(Mean),
-           SD = as.numeric(SD),
-           X..CV = as.numeric(X..CV))
-
-  for(i in 1:nrow(hist_bas)){
-    hist_bas$Mean_format[i] <- formatC(hist_bas$Mean[i] ,
-                                       digits = hist_bas$digits[i],
-                                       format = num_format)
-    hist_bas$SD_format[i] <- formatC(hist_bas$SD[i],
-                                     digits = hist_bas$digits[i],
-                                     format = num_format)
-    hist_bas$`% CV_format`[i] <- formatC(hist_bas$X..CV[i],
-                                         digits = hist_bas$digits[i],
-                                         format = num_format)
-  }
+    left_join(df_round) %>%
+    mutate_at(c("Mean", "SD", "% CV"),
+              ~ case_when(!is.infinite(round_val) ~ round(.x, round_val),
+                          TRUE ~ .x))
 
 
   merged_values <- left_join(hist_bas, hist_ext) %>%
     left_join(hist_per) %>%
-    select(station:N, ends_with("_format"))
+    select(station:N, Mean:"% CV", Min:Max, "10%":"90%")
 
   if(by_month == F){
-    names(merged_values) <- c(names(merged_values)[1:4],
-                              gsub("_format", "", names(merged_values)[5:9]),
-                              "10%", "25%", "50%", "75%", "90%")
-
     caption.text = paste0("Overall historical summary (", min(df$year), "-",
                           max(df$year), ") for ", sampling_station, " ",
                           grouping, " water quality characteristics.")
   } else {
-
-    names(merged_values) <- c(names(merged_values)[1:5],
-                              gsub("_format", "", names(merged_values)[6:10]),
-                              "10%", "25%", "50%", "75%", "90%")
-
     merged_values <- merged_values %>%
       mutate(month = factor(month, ordered = T, levels = month.abb)) %>%
       filter(month == month_select)
@@ -209,6 +213,24 @@ group_table_historical <- function(df = "", year_select = "", grouping = "",
                          max(df$year), ".")
   }
 
+  if (grouping == "biological") {
+    merged_values <- merged_values %>%
+      mutate_at(c("Mean", "SD", "% CV", "Min", "Max",
+                  "10%", "25%", "50%", "75%", "90%"),
+                ~ formatC(.x, format = "e"))
+  }
+
+
+  merged_values <- merged_values %>%
+    mutate_at(c("Mean", "SD", "% CV", "Min", "Max",
+                "10%", "25%", "50%", "75%", "90%"),
+              ~ as.character(.x),
+              c("Mean", "SD", "% CV", "Min", "Max",
+                "10%", "25%", "50%", "75%", "90%"),
+              ~ case_when(gre ~ round(.x, round_val),
+                          TRUE ~ .x))
+
+
   replace_list <- c("NA","NaN","-Inf","Inf")
 
   ## stopiing::need to replace values
@@ -216,12 +238,11 @@ group_table_historical <- function(df = "", year_select = "", grouping = "",
     merged_values[merged_values == replace_list[i]] <- "--"
   }
 
-
-
   pander(merged_values[, c(2,3,5:ncol(merged_values))],
          justify = c("left", "center", rep("right", ncol(merged_values) - 4)),
          style = "rmarkdown", split.table = Inf,
          caption = caption.text, graph.fontsize = 9)
+
 }
 
 
